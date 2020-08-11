619dff20ca522f0b2c35e4e719077bb8f401c658
mritunjaysaha <59063979+mritunjaysaha@users.noreply.github.com>
2020-08-11 17:57:47 +0530

    added method to add data to table

--- app.js
+++ app.js
@@ -1,72 +1,74 @@
-if (process.env.NODE_ENV !== 'production') {
-    require('dotenv').config()
+if (process.env.NODE_ENV !== "production") {
+    require("dotenv").config();
 }
 
-
-const express = require('express');
+const express = require("express");
 const app = express();
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const mongoose = require('mongoose');
-const passport = require('passport');
-const session = require('express-session');
-const flash = require('express-flash');
-const expressLayouts = require('express-ejs-layouts');
-const methodOverride = require('method-override');
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const passport = require("passport");
+const session = require("express-session");
+const flash = require("express-flash");
+const expressLayouts = require("express-ejs-layouts");
+const methodOverride = require("method-override");
 
+mongoose
+    .connect(process.env.DATABASE_URL, {
+        useCreateIndex: true,
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useFindAndModify: true,
+    })
+    .then((success) => console.log("Successfully connected"))
+    .catch((err) => console.log(`Error Occured at ${err}`));
 
-mongoose.connect(process.env.DATABASE_URL , {useCreateIndex: true , useNewUrlParser: true , useUnifiedTopology: true , useFindAndModify: true})
-    .then (success => console.log('Successfully connected'))
-    .catch(err => console.log(`Error Occured at ${err}`));
-
-
-// Set Up view engine 
-app.set('view engine' , 'ejs');
+// Set Up view engine
+app.set("view engine", "ejs");
 
 // Set Public and static files directory;
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 // body parser
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
-
-
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(flash());
-app.use(session({
-    secret:process.env.SESSION_SECRET,
-    resave:false,
-    saveUninitialized:false
-}))
+app.use(
+    session({
+        secret: process.env.SESSION_SECRET,
+        resave: false,
+        saveUninitialized: false,
+    })
+);
 
-// ejs Layouts 
-app.set('layout' , 'layouts/layout')
-app.use(expressLayouts)
+// ejs Layouts
+app.set("layout", "layouts/layout");
+app.use(expressLayouts);
 
-// Passport setup 
+// Passport setup
 app.use(passport.initialize());
 app.use(passport.session());
 
-// Method Override 
-app.use(methodOverride('_method'))
+// Method Override
+app.use(methodOverride("_method"));
 
-// Import MiddleWares 
-const {isSignedIn , } = require('./middlewares/auth');
+// Import MiddleWares
+const { isSignedIn } = require("./middlewares/auth");
 
-// All Routes 
-const homeRouter = require('./controllers/home');
-const mainRouter = require('./controllers/main');
-const productRouter = require('./controllers/product');
-const orderRouter = require('./controllers/order');
+// All Routes
+const homeRouter = require("./controllers/home");
+const mainRouter = require("./controllers/main");
+const productRouter = require("./controllers/product");
+const orderRouter = require("./controllers/order");
 
-app.use('/' , homeRouter);
-app.use('/main' , isSignedIn ,  mainRouter);
-app.use('/products' , isSignedIn, productRouter);
-app.use('/orders' , orderRouter);
+app.use("/", homeRouter);
+app.use("/main", isSignedIn, mainRouter);
+app.use("/products", isSignedIn, productRouter);
+app.use("/orders", orderRouter);
 
+const PORT = process.env.PORT || 3000;
 
-
-
-app.listen(process.env.PORT || 3000 , () => {
-    console.log(`Server Started on port`);
-} )
+app.listen(PORT, () => {
+    console.log(`Server Started on port http://localhost:${PORT}`);
+});
--- public/scripts/filterableproducts.js
+++ public/scripts/filterableproducts.js
@@ -1,32 +1,71 @@
-const search = document.getElementById('search');
-const productContainer = document.querySelector('.match-list')
-const matchUl = document.querySelector('.ul');
-const productMain = document.querySelector('.product');
-search.addEventListener('input' , async (e) => {
-    console.log('yes');
-    console.log(search.value)
-     fetch('http://localhost:3000/orders/search' + '?' + new URLSearchParams({
-         name:`${search.value}`
-     }))  
-     .then(res => res.json())
-     .then(data => {
-         if (search.value == '' || null) {
-             matchUl.innerHTML = ''
-         } else {
-            data.products.forEach((product , index )  => {
-                let html = '';
-                html = ` 
-                    <li><a class="product" onclick="productshow(${product})">${product.product_name}</a></li> `
-                
-                matchUl.innerHTML += html ;
-                 
-             })
-         }
-      
-     })
-      
+const search = document.getElementById("search");
+const productContainer = document.querySelector(".match-list");
+const matchUl = document.querySelector(".ul");
+const productMain = document.querySelector(".product");
+const productsQuery = document.querySelector("#productsgohere");
+
+console.log({ productsQuery });
+productsQuery.style.background = "#f00";
+const productsArr = [
+    { id: "kajsdkla", first: "jsdklad", second: "dsada", handle: "dadasd" },
+];
+
+search.addEventListener("input", async (e) => {
+    console.log("yes");
+    console.log(search.value);
+    fetch(
+        "http://localhost:3000/orders/search" +
+            "?" +
+            new URLSearchParams({
+                name: `${search.value}`,
+            })
+    )
+        .then((res) => res.json())
+        .then((data) => {
+            if (search.value == "" || null) {
+                matchUl.innerHTML = "";
+            } else {
+                data.products.forEach((product, index) => {
+                    let html = "";
+                    html = ` 
+                    <li><a class="product" onclick="productshow(${product})">${product.product_name}</a></li> `;
+
+                    matchUl.innerHTML += html;
+                });
+            }
+        });
 });
 
-function productshow ( product ) {
+/**
+ * Pass the objects to this function and it will add
+ * the data to the table
+ * @param {object} arr
+ */
+function addProducts(arr) {
+    const trElement = document.createElement("tr");
+
+    const tdElement1 = document.createElement("td");
+    const tdElement2 = document.createElement("td");
+    const tdElement3 = document.createElement("td");
+    const tdElement4 = document.createElement("td");
+
+    console.log(arr.id);
+
+    tdElement1.innerText = arr.id;
+    tdElement2.innerText = arr.first;
+    tdElement3.innerText = arr.second;
+    tdElement4.innerText = arr.handle;
+
+    trElement.appendChild(tdElement1);
+    trElement.appendChild(tdElement2);
+    trElement.appendChild(tdElement3);
+    trElement.appendChild(tdElement4);
+
+    productsQuery.appendChild(trElement);
+}
+
+function productshow(product) {
     console.log(product);
 }
+
+addProducts(productsArr[0]);
